677e968d7938609494267dd4f2a2b3e0544792b4
Hendry <email.hendry.lee@gmail.com>
2016-01-02 19:35:17 +0700

    Create method to go to loading page

--- src/app/lobby/lobby.controller.js
+++ src/app/lobby/lobby.controller.js
@@ -1,13 +1,28 @@
 class LobbyController {
-  constructor($scope, $state, $stateParams) {
+  constructor($scope, $state, $stateParams, SocketService) {
     'ngInject';
 
+    this.$state = $state;
+    this.SocketService = SocketService;
     this.roomName = $stateParams.roomName;
 
     $scope.$on('owner_disconnect', function(event, args) {
       alert('Computer disconnected from the game. Game ended.');
       $state.go('main');
     })
+  }
+
+  startGame() {
+    this.SocketService.send({
+      type:'players_ready',
+      role:'player'
+    });
+
+    this.SocketService.extendedHandler = (message) => {
+      if(message.type === 'players_ready') {
+        this.$state.go('loading');
+      }
+    }
   }
 }
 
