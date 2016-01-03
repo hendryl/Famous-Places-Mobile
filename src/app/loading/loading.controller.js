5f2b7f08bd5c3a3fe9cd76e3ae6cc8d7b30b9db1
Hendry <email.hendry.lee@gmail.com>
2016-01-03 20:56:00 +0700

    Create loading page layout

--- src/app/loading/loading.controller.js
+++ src/app/loading/loading.controller.js
@@ -1,11 +1,24 @@
 class LoadingController {
-  constructor($scope, $state) {
+  constructor($scope, $state, $interval) {
     'ngInject';
+
+    this.animTime = 850;
+    this.ellipsis = '.';
 
     $scope.$on('server_disconnect', function(event, args) {
       alert('Server disconnected. Game ended.');
       $state.go('main');
     });
+
+    $interval(() => this.changeEllipsis(), this.animTime, 0, true);
+  }
+
+  changeEllipsis() {
+    this.ellipsis += '.';
+
+    if (this.ellipsis.length > 3) {
+      this.ellipsis = '.';
+    }
   }
 }
 
