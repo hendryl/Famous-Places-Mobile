ba1d788be6683dcd379f469293e47473613cc2ad
Hendry <hendry@icehousecorp.com>
2016-01-04 13:26:18 +0700

    Add disconnect event checker to game page

--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -1,6 +1,11 @@
 class GameController {
-  constructor() {
+  constructor($scope) {
     'ngInject';
+
+    $scope.$on('server_disconnect', function(event, args) {
+      alert('Server disconnected. Game ended.');
+      $state.go('main');
+    });
   }
 }
 
