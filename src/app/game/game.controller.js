47b5591c2e4483f3a3380ae66d126747f2ac731b
Hendry <hendry@icehousecorp.com>
2016-01-06 12:48:25 +0700

    Create handlers for marker events

--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -1,70 +1,63 @@
 class GameController {
-  constructor($scope, $state, NgMap, mapsKey) {
+  constructor($log, $scope, $state, NgMap, mapsKey) {
     'ngInject';
 
+    this.latLngDecimals = 6;
+    this.$log = $log;
     this.$state = $state;
 
     this.googleMapsURL = "https://maps.google.com/maps/api/js?libraries=places&callback=prepareMap&key=" + mapsKey;
 
     this.instruction = "Ready";
     this.hasSubmitted = false;
+    this.map = null;
 
     this.mapCenter = {
       lat: 0,
       long: 0
-    }
+    };
 
     this.answer = {
       lat: this.mapCenter.lat,
       long: this.mapCenter.long
-    }
+    };
 
     $scope.$on('server_disconnect', function(event, args) {
       alert('Server disconnected. Game ended.');
       $state.go('main');
     });
+
+    this.$log.log(this.answer);
   }
 
   prepareMap(map) {
     this.map = map;
 
-    const styles = [
-      {
-        featureType: "poi",
-        elementType: "labels.icon",
-        stylers: [
-          {
-            visibility: "off"
-          }
-        ]
-      }
-    ]
+    const styles = [{
+      featureType: "poi",
+      elementType: "labels.icon",
+      stylers: [{
+        visibility: "off"
+      }]
+    }]
     const options = {
       streetViewControl: false,
+      mapTypeControl: false,
       styles: styles
     };
 
     map.setOptions(options);
   }
 
-  dragMap(event) {
-
+  placeMarker(event, game) {
+    this.panTo(event.latLng);
+    game.moveMarker(event, game);
   }
 
-  placeMarker(event) {
-
-  }
-
-  moveMarker(event) {
-
-  }
-
-  startMoveMarker(event) {
-
-  }
-
-  endMoveMarker(event) {
-
+  //NgMap's eventing binds this to the map, so we have to get the controller
+  moveMarker(event, game) {
+    game.answer.lat = event.latLng.lat().toFixed(game.latLngDecimals);
+    game.answer.long = event.latLng.lng().toFixed(game.latLngDecimals);
   }
 }
 
