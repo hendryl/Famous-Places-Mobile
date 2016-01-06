c4a644fe9f96c80dd7edfef96845c65cca461582
Hendry <hendry@icehousecorp.com>
2016-01-06 10:11:48 +0700

    Fix no $state error 

--- src/app/game/game.controller.js
+++ src/app/game/game.controller.js
@@ -1,6 +1,8 @@
 class GameController {
-  constructor($scope) {
+  constructor($scope, $state) {
     'ngInject';
+
+    this.$state = $state;
 
     $scope.$on('server_disconnect', function(event, args) {
       alert('Server disconnected. Game ended.');
