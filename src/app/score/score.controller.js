888b85bb98e7debd3b7db17348d426fbe0ebf2ef
Hendry <hendry@icehousecorp.com>
2016-01-08 11:08:31 +0700

    Handle stuff needed in controller

--- src/app/score/score.controller.js
+++ src/app/score/score.controller.js
@@ -1,13 +1,41 @@
 class ScoreController {
-  constructor($scope, $state, SocketService) {
+  constructor($scope, $state, $window, SocketService) {
     'ngInject';
+
+    this.$window = $window;
+
+    this.text = 'Waiting for results';
+    this.haveNextRound = true;
+    this.canContinue = false;
 
     $scope.$on('server_disconnect', function(event, args) {
       alert('Server disconnected. Game ended.');
       $state.go('main');
     });
 
-    this.text = 'Waiting for results';
+
+    this.SocketService.extendedHandler = (message) => {
+      if (message.type === 'end_score') {
+        $window.navigator.vibrate(200);
+
+        this.canContinue = true;
+        this.haveNextRound = message.haveNextRound;
+
+        if (this.haveNextRound) {
+          this.text = 'End Game';
+        } else {
+          this.text = 'Next Question';
+        }
+      }
+    };
+
+    this.prepareVibration();
+  }
+
+  prepareVibration() {
+    const navigator = this.$window.navigator;
+
+    navigator.vibrate = navigator.vibrate || navigator.webkitVibrate || navigator.mozVibrate || navigator.msVibrate;
   }
 }
 
