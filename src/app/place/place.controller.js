ca7bba8762ffbf3edfdea43d927c0b03bfdf1222
Hendry <hendry@icehousecorp.com>
2016-01-14 15:39:06 +0700

    Create info page for place

--- src/app/place/place.controller.js
+++ src/app/place/place.controller.js
@@ -1,11 +1,21 @@
 class PlaceController {
-  constructor($log, $stateParams) {
+  constructor($log, $stateParams, $state, $window) {
     'ngInject';
 
     this.$log = $log;
     this.info = $stateParams.info;
+    this.$state = $state;
+    this.$window = $window;
 
     $log.debug(this.info);
+  }
+
+  openLearnMore() {
+    this.$window.open(this.info.link, '_blank');
+  }
+
+  back() {
+    this.$state.go('result');
   }
 }
 
